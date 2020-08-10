963d887bebf4430b9417b7ed33f6b2f0bc660e96
MarkHu0423 <1512571700@qq.com>
2020-08-10 21:22:38 +0800

    石头剪刀布代码

---
+++ game.py
@@ -0,0 +1,31 @@
+import random
+secret = random.randint(1,3)
+number = secret
+
+print("----------------Game Start!----------------")
+while number == secret :
+    number = int (input("石头请按1,剪刀请按2,布请按3:"))   
+    if secret == number:
+        print("平局,再来")
+        secret = random.randint(1,3)
+        number = secret
+
+    elif secret == 1 and number == 2:
+        print("电脑出石头,你输了")
+    elif secret == 1 and number == 3:
+        print("电脑出石头,你赢了")
+
+    elif secret == 2 and number == 3:
+        print("电脑出剪刀,你输了")
+    elif secret == 2 and number == 1:
+        print("电脑出剪刀,你赢了")
+
+    elif secret == 3 and number == 1:
+        print("电脑出布,你输了")
+    elif secret == 3 and number == 2:
+        print("电脑出布,你赢了")
+
+    else:
+        print("请输入1到3的数字.")
+        number = secret
+print("----------------Game Over!----------------")
