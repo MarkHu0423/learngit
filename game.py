b58419847f46f9633258583ea98f3c064ff399fd
MarkHu0423 <1512571700@qq.com>
2020-08-10 23:38:05 +0800

    第三次修改

--- game.py
+++ game.py
@@ -30,5 +30,5 @@ while number == secret :
     else:
         print("请输入1到3的数字.")
         number = secret
-        # 对输入不规范的行为进行提醒
+        # 对不规范的输入进行提醒
 print("----------------Game Over!----------------")
