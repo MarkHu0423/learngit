f38cae61ecbd7c9a9263e3227be123ff030a5436
MarkHu0423 <1512571700@qq.com>
2020-08-12 11:38:43 +0800

    格式规范

--- game.py
+++ game.py
@@ -1,34 +1,26 @@
 import random
-secret = random.randint(1,3)
-# 模拟电脑随机出拳
-number = secret
-
+Computer = random.randint(1,3)
+number = Computer
 print("----------------Game Start!----------------")
-while number == secret :
+while number == Computer :
     number = int (input("石头请按1,剪刀请按2,布请按3:"))   
-    if secret == number:
+    if Computer == number:
         print("平局,再来")
-        secret = random.randint(1,3)
-        number = secret
-
-    elif secret == 1 and number == 2:
+        Computer = random.randint(1,3)
+        number = Computer
+    elif Computer == 1 and number == 2:
         print("电脑出石头,你输了")
-    elif secret == 1 and number == 3:
+    elif Computer == 1 and number == 3:
         print("电脑出石头,你赢了")
-
-    elif secret == 2 and number == 3:
+    elif Computer == 2 and number == 3:
         print("电脑出剪刀,你输了")
-    elif secret == 2 and number == 1:
+    elif Computer == 2 and number == 1:
         print("电脑出剪刀,你赢了")
-
-    elif secret == 3 and number == 1:
+    elif Computer == 3 and number == 1:
         print("电脑出布,你输了")
-    elif secret == 3 and number == 2:
+    elif Computer == 3 and number == 2:
         print("电脑出布,你赢了")
-        #列出所有输赢情况
-
     else:
         print("请输入1到3的数字.")
-        number = secret
-        # 对不规范的输入进行提醒
+        number = Computer
 print("----------------Game Over!----------------")
